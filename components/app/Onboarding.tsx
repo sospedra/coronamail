af78eab3046a293a9012122944fd6d5c23e53629
Rubén Sospedra <hello@sospedra.me>
2020-04-10 19:01:43 +0200

    feat: Style onboarding for mobile

--- components/app/Onboarding.tsx
+++ components/app/Onboarding.tsx
@@ -1,7 +1,8 @@
 import React, { useRef, useState, useEffect } from 'react'
 import ReactSwipe from 'react-swipe'
 import { range } from 'lodash'
-import { matchMedia } from 'services/styles'
+import { matchMedia, styles } from 'services/styles'
+import { useTranslation } from 'services/i18n'
 
 const STEPS = [
   {
@@ -16,7 +17,6 @@ const STEPS = [
     border: 'bg-red-600',
     img: '',
     title: 'red title',
-    description: 'red description',
   },
   {
     bg: 'bg-indigo-800',
@@ -28,7 +28,9 @@ const STEPS = [
 ]
 
 const Onboarding: React.FC<{}> = () => {
+  const { t } = useTranslation()
   const swipeRef = useRef<ReactSwipe>(null)
+  const [isVisible, setIsVisible] = useState(true)
   const [position, setPosition] = useState(0)
   const [swipeOptions, setSwipeOptions] = useState<object>({
     continuous: false,
@@ -37,12 +39,14 @@ const Onboarding: React.FC<{}> = () => {
   })
 
   useEffect(() => {
-    if (!matchMedia('sm')) {
+    if (matchMedia('sm')) {
       setSwipeOptions({
         continuous: false,
       })
     }
   }, [])
+
+  if (!isVisible) return null
 
   return (
     <aside className='fixed top-0 bottom-0 left-0 right-0 z-40 flex items-center justify-center w-screen onboarding'>
@@ -108,7 +112,7 @@ const Onboarding: React.FC<{}> = () => {
             className={`mobile-paddings sm:hidden ${
               position === 0
                 ? 'bg-indigo-500 border-indigo-600 '
-                : 'bg-orange-500 border-orange-600'
+                : 'bg-indigo-800 border-indigo-900'
             }`}
           ></div>
           <ReactSwipe
@@ -118,13 +122,20 @@ const Onboarding: React.FC<{}> = () => {
           >
             {STEPS.map(({ bg, border, title, description }, i) => (
               <div className='flex flex-col flex-1 h-full' key={i}>
-                <div className={`flex flex-1 p-4 sm:rounded-t ${bg}`}>
-                  {title}
-                </div>
+                <div className={`flex flex-1 p-4 ${bg}`}>{title}</div>
                 <div
-                  className={`flex h-48 p-4 pb-12 sm:rounded-b sm:pb-4 ${border}`}
+                  className={`flex flex-col h-48 p-4 pb-12 sm:pb-4 ${border}`}
                 >
-                  {description}
+                  <div className='flex flex-1'>{description}</div>
+
+                  {i === STEPS.length - 1 && (
+                    <button
+                      onClick={() => setIsVisible(false)}
+                      className='self-center hidden px-2 py-4 text-lg text-gray-700 bg-white rounded shadow-md sm:block'
+                    >
+                      {t('app', 'onboarding', 'start')}
+                    </button>
+                  )}
                 </div>
               </div>
             ))}
@@ -146,15 +157,54 @@ const Onboarding: React.FC<{}> = () => {
             </svg>
           </button>
         </div>
-        <div className='absolute bottom-0 left-0 right-0 z-30 flex flex-row justify-center py-4 text-gray-200 sm:relative'>
-          {range(STEPS.length).map((i) => (
+        <div className='absolute bottom-0 left-0 right-0 z-30 flex flex-row items-center px-4 py-4 text-gray-200 sm:relative'>
+          <div className='flex flex-row items-center flex-1 sm:justify-center'>
             <button
-              onClick={() => swipeRef.current?.slide(i, 300)}
-              className={`w-2 h-2 m-1 bg-white rounded-full focus:outline-none focus:shadow-outline ${
-                i === position ? '' : 'opacity-50'
-              }`}
-            ></button>
-          ))}
+              className='w-5 h-5 sm:hidden'
+              onClick={() => swipeRef.current?.prev()}
+            >
+              {position !== 0 && (
+                <svg
+                  focusable='false'
+                  viewBox='0 0 24 24'
+                  aria-hidden='true'
+                  role='presentation'
+                >
+                  <path fill='none' d='M0 0h24v24H0z'></path>
+                  <path
+                    fill={styles.theme.colors.gray[200]}
+                    d='M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z'
+                  ></path>
+                </svg>
+              )}
+            </button>
+            <div className='flex flex-row mx-2'>
+              {range(STEPS.length).map((i) => (
+                <button
+                  key={i}
+                  onClick={() => swipeRef.current?.slide(i, 300)}
+                  className={`w-2 h-2 m-1 bg-white rounded-full focus:outline-none focus:shadow-outline ${
+                    i === position ? '' : 'opacity-50'
+                  }`}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className='flex justify-end flex-1 sm:hidden'>
+            <button
+              className='px-2 py-1 text-sm text-gray-700 bg-white rounded shadow-md'
+              onClick={() => {
+                position === STEPS.length - 1
+                  ? setIsVisible(false)
+                  : swipeRef.current?.next()
+              }}
+            >
+              {position === STEPS.length - 1
+                ? t('app', 'onboarding', 'start')
+                : t('app', 'onboarding', 'next')}
+            </button>
+          </div>
         </div>
       </div>
     </aside>
--- services/styles/index.ts
+++ services/styles/index.ts
@@ -6,7 +6,6 @@ export const styles = resolveConfig(tailwindConfig)
 export const matchMedia = (screen: 'sm' | 'md' | 'lg' | 'xl') => {
   if (window && window.matchMedia) {
     const mq = window.matchMedia(`(min-width: ${styles.theme.screens[screen]})`)
-    console.log(mq)
     return !!mq.matches
   } else {
     return false
