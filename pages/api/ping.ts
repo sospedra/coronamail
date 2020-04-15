e0e912c03779f7c8b58b92e65dcbda9afa3a7e6c
Rubén Sospedra <hello@sospedra.me>
2020-04-15 19:03:50 +0200

    feat: Remove ping api

--- pages/api/ping.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiResponse, NextApiRequest } from 'next'
-
-export default async (_: NextApiRequest, res: NextApiResponse) => {
-  return res.status(200).json({ data: { ping: 'pong' } })
-}
